c513ebe36934bd74a536e3b000abaa8e1f7c18fb
245harshit <77959313+245harshit@users.noreply.github.com>
2022-02-19 18:14:48 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "secretkey123"
+app.config['SECRET_KEY'] = 'hais i*#) #*# #*(@9 iewd (2(((!9309-'
 
 @app.route("/")  # this sets the route to this page
 def home():
